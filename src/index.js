57ccdf5ba3e5ccd5f4e131f15dcc714ccc46a808
MSA <crystalbluetech@gmail.com>
2023-11-27 10:39:55 +0100

    update info

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "./app.css";
+import "./App.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
