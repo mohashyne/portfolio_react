5072608a01f441b0c5a25f9f9d29f3f71d5f213f
MSA <crystalbluetech@gmail.com>
2023-12-01 10:31:31 +0100

    add project images

--- src/components/projects/project.jsx
+++ src/components/projects/project.jsx
@@ -2,42 +2,40 @@ import React from "react";
 import "./styles/project.css";
 
 const Project = (props) => {
-	const { logo, logo2, logo3, title, description, link, linkSource,image } = props;
+  const { logo, logo2, logo3, title, description, link, linkSource, image, projectCover } = props;
 
-	return (
-		<React.Fragment>
-			<div className="project">
-				
-					<div className="project-container">
-						<div className="project-logo">
-							<img src={logo} alt="logo" />
-							<img src={logo2} alt="logo" />
-							<img src={logo3} alt="logo" />
-						</div>
-						<div className="project-title">{title}</div>
-						<div className="project-image">
-							<img src={image} alt="project_image" />
-						</div>
-						<div className="project-description">{description}</div>
-						<div className="project-link">
-                  <div className="project-btn">
-										<a href={linkSource} target="_blank" rel="noopener noreferrer" className="source-project-button">
-												View Source
-										</a>
-										<a href={link} target="_blank" rel="noopener noreferrer" className="view-project-button">
-                       View Project
-                    </a>
-
-									</div>    
-                  
-
-									
-                </div>
-					</div>
-				
-			</div>
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <div className="project">
+        <div className="project-container">
+          <div className="project-logo">
+            <img src={logo} alt="logo" />
+            <img src={logo2} alt="logo" />
+            <img src={logo3} alt="logo" />
+          </div>
+          <div className="project-title">{title}</div>
+          {/* Add a div for the pop-up */}
+          <div className="project-popup">
+            <img src={projectCover} alt="project_cover" />
+          </div>
+          <div className="project-image">
+            <img src={image} alt="project_image" />
+          </div>
+          <div className="project-description">{description}</div>
+          <div className="project-link">
+            <div className="project-btn">
+              <a href={linkSource} target="_blank" rel="noopener noreferrer" className="source-project-button">
+                View Source
+              </a>
+              <a href={link} target="_blank" rel="noopener noreferrer" className="view-project-button">
+                View Project
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Project;
--- src/data/user.js
+++ src/data/user.js
@@ -47,7 +47,7 @@ const INFO = {
 			image: "/project1.png",
 			technologies: ["JavaScript", "React", "Redux"],
 			sourceLink: "https://github.com/mohashyne/book_store",
-			projectCover: "./Snapshoot_Portfolio_1.png",
+			projectCover: "/Snapshoot_Portfolio_1.png",
 			fullDescription: `Book Store is a web application that provides a platform for users to browse , add and delete books. The bookstore features a user-friendly interface with a wide selection of books, categorized for easy navigation
           `,
 			canopy: ["Full Stack Dev", 2023],
@@ -67,7 +67,7 @@ const INFO = {
 				"https://github.com/mohashyne/24hour_doctor-front_end.git",
 			image: "/project2.png",
 			technologies: ["React", "Redux", "JavaScript", "Rails"],
-			projectCover: "./Snapshoot_Portfolio_2.svg",
+			projectCover: "/Snapshoot_Portfolio_2.png",
 			fullDescription: `
 			24-Hour Doctor Reservation App is a modern and dynamic web application designed to manage and display a directory of doctors and their reservations. The application offers an intuitive and user-friendly interface, allowing users to view detailed information about doctors, as well as to add, update, and delete doctor records interactively.
 				  `,
@@ -87,7 +87,7 @@ const INFO = {
 			image: "/project3.png",
 			technologies: ["HTML", "CSS", "JavaScript"],
 			sourceLink: "https://github.com/mohashyne/Module-Capstone--2",
-			projectCover: "./Snapshoot_Portfolio_3.png",
+			projectCover: "/project3_cover.png",
 			fullDescription: `
 			World Vista is a dynamic web app using the Fetch API for country data. It offers details on culture, history, and attractions. Users can book tours, share comments, and like countries. Explore the world and connect with fellow travelers on World Vista.
 				  `,
@@ -107,7 +107,7 @@ const INFO = {
 			image: "/project4.png",
 			technologies: ["HTML", "CSS", "JavaScript"],
 			sourceLink: "https://github.com/mohashyne/space-travelers-hub",
-			projectCover: "./Snapshoot_Portfolio_4.png",
+			projectCover: "/Snapshoot_Portfolio_4.png",
 			fullDescription: `
 			Space Travelers Hub is a group project developed by Muhammad and Henok. The web application allows users to reserve and cancel rocket reservations while offering an enhanced user interface for visual clarity. The mission features include seamless action dispatching for rocket reservations and cancellations
 				  `,
@@ -127,7 +127,7 @@ const INFO = {
 			image: "/project5.png",
 			technologies: ["React", "Redux", "JavaScript"],
 			sourceLink: "https://github.com/mohashyne/metric-web_apps-capstone-.git",
-			projectCover: "./Snapshoot_Portfolio_5.png",
+			projectCover: "/project5.png",
 			fullDescription: `
 			[metric-web_apps] metric-web_apps is a mobile application built using React and Redux. It empowers users to explore countries and cities, offering access to comprehensive air quality information for each city. This data is sourced from an external API, enhancing user awareness of air quality conditions.
 				  `,
